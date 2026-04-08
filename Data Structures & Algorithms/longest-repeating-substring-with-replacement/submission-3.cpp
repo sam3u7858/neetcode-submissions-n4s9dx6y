3b3a1297a77c407d3bcaf314b265964935a2e5f7
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-04-08 23:06:02 +0800

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        unordered_map<char, int> freq;
+        int maxCount = 0;
+        int res=0;
+
+        for(int r=0; r<s.size(); r++){
+
+            freq[s[r]]++;
+            maxCount = max(maxCount, freq[s[r]]);
+            
+            while((r-l+1)-maxCount > k){
+                freq[s[l]]--;
+                l++;
+            }  
+
+           
+            
+            res = max(r-l+1, res);
+
+
+
+        }
+        return res;
+
+    }
+};

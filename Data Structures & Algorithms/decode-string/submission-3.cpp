87c1f263f4e6d38e591f1287a3e324a657842ccb
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-04-26 17:13:44 +0800

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> counts;
+        stack<string> pattern;
+
+        int k = 0;
+        string currstr = "";
+        currstr.reserve(s.size()*2);
+
+        for(const char& c: s){
+           
+
+            if(isdigit(c)){
+
+                k = k * 10 + (c-'0');
+
+            }
+
+            else if(c == '['){
+                counts.push(k);
+                pattern.push(currstr);
+                currstr = "";
+                k = 0;
+
+            }
+            else if(c == ']'){
+                string tmp = currstr;
+
+                currstr = pattern.top();
+                pattern.pop();
+                int repeats = counts.top();
+                counts.pop();
+
+                while(repeats-- ){
+                    currstr += tmp;
+                }
+            }
+            else{
+                currstr += c;
+            }
+        }
+
+        return currstr;
+
+    }
+};

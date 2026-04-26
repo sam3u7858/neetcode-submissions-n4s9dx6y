a87304af8e68e3e6221a34aad920e841520d9753
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-04-26 18:47:40 +0800

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.cpp
@@ -0,0 +1,35 @@
+class FreqStack {
+public:
+    unordered_map<int, stack<int>> freqStack;
+    unordered_map<int, int> freq;
+    int maxFreq;
+
+    FreqStack() {
+        maxFreq = 1;
+        
+    }
+    
+    void push(int val) {
+
+        freq[val]+=1;
+        freqStack[freq[val]].push(val);
+        maxFreq = max(maxFreq, freq[val]);
+        
+    }
+    
+    int pop() {
+
+        int val = freqStack[maxFreq].top(); freqStack[maxFreq].pop();
+        freq[val] -= 1;
+        
+        if(freqStack[maxFreq].empty()) maxFreq--;     
+        return val;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */

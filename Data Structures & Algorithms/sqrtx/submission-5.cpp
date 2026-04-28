f1a6deeabb85a2577beff611d6f7ce4a50ccc13b
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-04-28 17:43:50 +0800

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+
+    int mySqrt(int x) {
+        int res = 0;
+        if(x==1) return 1;
+        if(x>46340*46340) return 46340;
+        
+
+        for(int i=0; i<x; i++){
+            long long tmp = i*i;
+            if(tmp == x) return i;
+            else if(tmp>x){
+                return i-1;
+            }
+
+        }
+
+        return 0;
+
+        
+
+    }
+};

ad0f97bd8dd03761f168570124a5a37659d56841
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-04-30 16:08:10 +0800

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    int checkTime(vector<int>& piles, int amount) {
+        int res = 0;
+        for (auto const& p : piles) {
+            res+= (p + amount - 1) / amount; 
+        }
+
+        return res;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int res = INT_MAX;
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+
+        while (l < r) {
+            int mid = (r - l) / 2 + l;
+            if (checkTime(piles, mid) > h)
+                l = mid + 1;
+            else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};

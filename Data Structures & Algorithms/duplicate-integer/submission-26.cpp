02f73d97878f67fa65a45ef9230176156abfb727
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-05-08 21:17:14 +0800

    Add: duplicate-integer - submission-26

---
+++ Data Structures & Algorithms/duplicate-integer/submission-26.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, bool> c;
+        for(const int& num:nums){
+            if(c.find(num) != c.end())return true;
+            c[num] = 1;
+
+            
+            
+        }
+        return false;
+
+
+    }
+};

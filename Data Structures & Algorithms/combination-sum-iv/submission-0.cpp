7d05550c2dfe89714969ac6c2323358111a90267
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-06-21 15:16:58 +0800

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        unordered_map<int, long long> dp;
+        dp[0] = 1;
+
+        for(int total =  1; total <= target; total++){
+            dp[total] = 0;
+            for(int num : nums){
+                if(total >= num){
+                    dp[total] += dp[total - num];
+                }
+            }
+
+            if(dp[total] > INT_MAX){
+                dp[total] = 0;
+            }
+
+        
+
+        }
+        
+
+        return dp[target];
+    }
+
+
+};

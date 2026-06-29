3244e48b4c6db11670b8a0797e9c9f53933032eb
sam3u7858 <60339835+sam3u7858@users.noreply.github.com>
2026-06-30 07:34:50 +0800

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+
+
+        int m = flowerbed.size();
+
+        
+        for(int i=0; i<m; i++){
+            if (n <= 0) return true;
+            if(flowerbed[i] == 0){
+
+                bool left = (i == 0 || flowerbed[i-1] == 0);
+                bool right = (i == m-1 || flowerbed[i+1] == 0);
+
+                if(left && right){
+                    flowerbed[i] = 1;
+                    n--;
+                }
+
+            }
+            
+        }
+        return n>0? false:true;
+    }
+};
